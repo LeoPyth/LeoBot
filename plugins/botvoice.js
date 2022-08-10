feff03fc00bfe7ed26185a9ad49fda493db68af8
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-08-10 14:37:23 +0700

    Update botvoice.js

--- plugins/botvoice.js
+++ plugins/botvoice.js
@@ -1,11 +1,11 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-let haibot = fs.readFileSync('./mp3/Bot.opus') 
+let haibot = fs.readFileSync('./mp3/sound1.mp3') 
 conn.sendFile(m.chat, haibot, '', '', m, true)
 }
 
-handler.customPrefix = /^(bot|robot)$/i
+handler.customPrefix = /^(.menu|.help)$/i
 handler.command = new RegExp
 
 handler.limit = true
