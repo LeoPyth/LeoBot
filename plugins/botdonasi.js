89a9f1af02109b522663e1d7aaffe2663a6a63b6
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-08-10 14:45:01 +0700

    Update botdonasi.js

--- plugins/botdonasi.js
+++ plugins/botdonasi.js
@@ -1,11 +1,11 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-let hellirfaan = fs.readFileSync('./mp3/Bot.opus') 
+let hellirfaan = fs.readFileSync('./mp3/sound3.mp3') 
 conn.sendFile(m.chat, hellirfaan, '', '', m, true)
 }
 
-handler.customPrefix = /^(bot|robot)$/i
+handler.customPrefix = /^(.donasi|.owner)$/i
 handler.command = new RegExp
 
 handler.limit = true
