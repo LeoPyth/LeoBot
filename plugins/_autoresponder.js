b1b5940b955b46db5daa88eb23169c3788be2a4e
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-08-13 21:02:32 +0700

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -5,8 +5,32 @@ let handler = m => m
 handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
 
+
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - */
-/* - - - - - - - Ini autoresponder by aine - - - - - - - */
+/* - - - - - - - Ini autoresponder - - - - - - - */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+let regj = /(.jadibot)/i
+    let isJadibot = regj.exec(m.text)
+    let jadbot = [
+'🤖',
+'🤖',
+'🤖'
+]
+let jadibot = jadbot[Math.floor(Math.random() * jadbot.length)]
+    if (isJadibot && !m.fromMe) {
+    conn.sendMessage(m.chat, {
+        react: {
+          text: `${jadibot}`,
+          key: m.key,
+        }})
+   setTimeout(() => {
+        conn.reply(m.chat, `mau jadibot? Ketik .sewa ${jadibot}`, m)
+    }, 1000)
+    }
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - */
+/* - - - - - - - Ini autoresponder - - - - - - - */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 let regs = /(aku cinta kamu)/i
@@ -29,7 +53,7 @@ let cintakamuh = cinmu[Math.floor(Math.random() * cinmu.length)]
     }
     
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - */
-/* - - - - - - - Ini autoresponder by aine - - - - - - - */
+/* - - - - - - - Ini autoresponder by arie - - - - - - - */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
     let regc = /(aku sayang kamu)/i
@@ -52,7 +76,7 @@ let sayangkamuh = saymu[Math.floor(Math.random() * saymu.length)]
     }
     
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - */
-/* - - - - - - - Ini autoresponder by aine - - - - - - - */
+/* - - - - - - - Ini autoresponder - - - - - - - */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
     let regl = /(I love you|love you)/i
@@ -91,7 +115,7 @@ let assl = assal[Math.floor(Math.random() * assal.length)]
     let assalamualaikum = fs.readFileSync('./mp3/WhatsApp-Audio-2021-03-02-at-20.46.15.opus')
     conn.sendMessage(m.chat, {
         react: {
-          text: `${assl}`,
+          text: `waalaikumsalam`,
           key: m.key,
         }})
     setTimeout(() => {
